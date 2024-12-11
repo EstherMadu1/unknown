4de01c449c480acab11872edf54abca465d22783
esther <esther.nmaduabuchi@gmail.com>
2024-12-11 05:41:53 +0100

    Remove ignored files from version control

--- instance/config.py
+++
@@ -1,5 +0,0 @@
-DBPASS='123456'
-SECRET_KEY='_IDTP7uMqmbsrg'
-SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root@localhost/harvesthub'
-#This is used to track modifications on the operations of your database
-SQLALCHEMY_TRACK_MODIFICATIONS=True
