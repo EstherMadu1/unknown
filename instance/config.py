8fd77942c0954f2c88a41d420a1a1d1cb3a21330
jaysinscars <jaysinscars@gmail.com>
2024-12-19 00:46:12 +0100

    fix

--- instance/config.py
+++ instance/config.py
@@ -1,5 +1,5 @@
-DBPASS = '123456'
-SECRET_KEY = '_IDTP7uMqmbsrg'
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://avnadmin:AVNS_NaPVH_s_Hc5VI4KRosm@mysql-2a75b001-jaysinscars-8d78.b.aivencloud.com:17636/teststoredb4'
-# This is used to track modifications on the operations of your database
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+DBPASS='123456'
+SECRET_KEY='_IDTP7uMqmbsrg'
+SQLALCHEMY_DATABASE_URI='mysql+mysqlconnector://root@localhost/harvesthub'
+#This is used to track modifications on the operations of your database
+SQLALCHEMY_TRACK_MODIFICATIONS=True
